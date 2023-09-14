d77f83faa0197cfd5e5e97c6f7962a96d338411e
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2023-09-14 15:04:25 +0500

    done

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
 import "./globals.css";
 import { Lato } from "next/font/google";
+import Image from "next/image";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Link from "next/link";
+import Logo from "./asstes/images/logo.png";
 const lato = Lato({
   subsets: ["latin"],
   variable: "--font-luckiest-guy",
@@ -15,45 +17,27 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-
-
   return (
     <html lang="en">
       <body className={`${lato.variable}`}>
-        <button
-          data-drawer-target="default-sidebar"
-          data-drawer-toggle="default-sidebar"
-          aria-controls="default-sidebar"
-          type="button"
-          className="absolute inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        >
-          <span className="sr-only">Open sidebar</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              clipRule="evenodd"
-              fillRule="evenodd"
-              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-            ></path>
-          </svg>
-        </button>
+        <div className="absolute inline-flex  items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg w-20 h-16">
+          <Image className="remative z-80" src={Logo} />
+        </div>
 
         <aside
           id="default-sidebar"
-          className="fixed top-0 left-0 z-40 w-48 h-screen transition-transform -translate-x-full sm:translate-x-0"
+          className="fixed top-0 left-0 border-r-[1px] z-40 w-48 h-screen transition-transform -translate-x-full sm:translate-x-0"
           aria-label="Sidebar"
         >
-          <div className="h-full px-3 py-4 overflow-y-auto bg-gray-100 dark:bg-gray-800">
-            <div className="space-y-2 font-medium h-full grid-rows-5">
-              <div>
+          <div className="h-full overflow-y-auto bg-gray-100 dark:bg-gray-800">
+            <div className="font-medium h-screen">
+              <div className="h-1/5 flex flex-col justify-center  w-full ">
+                <Image src={Logo} alt=""/>
+              </div>
+               <div className="h-1/5  w-full ">
                 <Link
                   href="/"
-                  className="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
+                  className="flex flex-col justify-center items-center h-full w-full text-gray-900 dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -70,13 +54,13 @@ export default function RootLayout({ children }) {
                     />
                   </svg>
 
-                  <span className="ml-3">Home</span>
+                  <span>Home</span>
                 </Link>
               </div>
-              <div>
+               <div className="h-1/5  w-full ">
                 <Link
                   href="/about"
-                  className="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
+                  className="flex flex-col items-center justify-center h-full w-full text-gray-900 dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -93,13 +77,13 @@ export default function RootLayout({ children }) {
                     />
                   </svg>
 
-                  <span className="flex-1 ml-3 whitespace-nowrap">About</span>
+                  <span className=" whitespace-nowrap">About</span>
                 </Link>
               </div>
-              <div>
+               <div className="h-1/5  w-full ">
                 <Link
                   href="/projects"
-                  className="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
+                  className="flex flex-col items-center justify-center h-full w-full text-gray-900  dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -116,15 +100,13 @@ export default function RootLayout({ children }) {
                     />
                   </svg>
 
-                  <span className="flex-1 ml-3 whitespace-nowrap">
-                    Projects
-                  </span>
+                  <span>Projects</span>
                 </Link>
               </div>
-              <div>
+               <div className="h-1/5  w-full ">
                 <Link
                   href="/contact"
-                  className="flex flex-col items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
+                  className="flex flex-col items-center justify-center h-full w-full text-gray-900  dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -141,24 +123,117 @@ export default function RootLayout({ children }) {
                     />
                   </svg>
 
-                  <span className="flex-1 ml-3 whitespace-nowrap">Contact</span>
+                  <span>Contact</span>
                 </Link>
               </div>
             </div>
           </div>
         </aside>
-        <div className=" sm:ml-48">
+        <div className=" mb-24 sm:mb-0 sm:ml-48">
           <div className=" border-gray-200   rounded-lg dark:border-gray-700">
             {children}
             <div className="absolute z-10">
-            <Footer/>
-
+              <Footer />
             </div>
           </div>
         </div>
 
+        <div className="fixed bottom-0 border-t-[1px] sm:hidden z-40 w-full h-24 overflow-y-auto bg-gray-100 dark:bg-gray-800">
+          <div className=" font-medium flex h-full items-center justify-center w-full">
+             <div className="h-1/5  w-full ">
+              <Link
+                href="/"
+                className="flex flex-col justify-center items-center h-full w-full text-gray-900 dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  />
+                </svg>
 
+                <span>Home</span>
+              </Link>
+            </div>
+             <div className="h-1/5  w-full ">
+              <Link
+                href="/about"
+                className="flex flex-col items-center justify-center h-full w-full text-gray-900 dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                  />
+                </svg>
 
+                <span className=" whitespace-nowrap">About</span>
+              </Link>
+            </div>
+             <div className="h-1/5  w-full ">
+              <Link
+                href="/projects"
+                className="flex flex-col items-center justify-center h-full w-full text-gray-900  dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
+                  />
+                </svg>
+
+                <span>Projects</span>
+              </Link>
+            </div>
+             <div className="h-1/5  w-full ">
+              <Link
+                href="/contact"
+                className="flex flex-col items-center justify-center h-full w-full text-gray-900  dark:text-white hover:bg-amber-400 dark:hover:bg-gray-700 group"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
+                  />
+                </svg>
+
+                <span>Contact</span>
+              </Link>
+            </div>
+          </div>
+        </div>
       </body>
     </html>
   );
